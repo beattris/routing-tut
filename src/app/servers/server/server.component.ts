d25caad685e5b247e1d4fafe8a5be0456164bb31
beattris <beatriceebirim@gmail.com>
2023-12-11 09:11:27 +0100

    used queryParams to edit the EditServer component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterLinkActive } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute ) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     // The '+' is added to convert the id from a string to a number, as in the servers array in ServerService
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
-    this.route.params.subscribe((params: Params) => { 
+    this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
     });
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route}); 
+  }
 }
